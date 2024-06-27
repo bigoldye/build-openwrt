2068733ea137c2231f060daf94ff4aef75fdf18d
bigoldye <58320970+bigoldye@users.noreply.github.com>
2024-06-28 00:42:37 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,8 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i 'src-git homeproxy https://github.com/bulianglin/homeproxy' feeds.conf.default
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
